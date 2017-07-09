853ab07a32a6773d9cfdc420aa075ede7492195d
Kengo Inaba <kengop@gmail.com>
2017-07-09 16:33:01 +0900

    first commit

---
+++ nyu1/nyu1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace nyu1
+{
+    public partial class nyu1Page : ContentPage
+    {
+        public nyu1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
