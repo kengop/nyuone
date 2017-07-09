12d617593c1695e2fc73760863a03cf940b840b1
Kengo Inaba <kengop@gmail.com>
2017-07-09 20:12:04 +0900

    リストを表示するViewの追加

---
+++ nyu1/Views/LogView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace nyu1.Views
+{
+    public partial class LogView : ContentPage
+    {
+        public LogView()
+        {
+            //BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            //var item = args.SelectedItem as Item;
+            //if (item == null)
+                //return;
+
+            //await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new NewItemPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //if (viewModel.Items.Count == 0)
+                //viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
