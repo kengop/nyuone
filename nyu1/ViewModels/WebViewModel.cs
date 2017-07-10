9097702b9b83301a3f51583188e6e080d78041a5
Kengo Inaba <kengop@gmail.com>
2017-07-11 00:35:25 +0900

    WebView確認用にWebViewModelを追加

---
+++ nyu1/ViewModels/WebViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace nyu1.ViewModels
+{
+    public class WebViewModel : BaseViewModel
+    {
+        public WebViewModel()
+        {
+            this.ViewSource = new Uri("https://www.xamarin.com");
+        }
+
+        private Uri m_viewSource;
+        public Uri ViewSource
+        {
+            get { return this.m_viewSource; }
+            set { SetProperty(ref this.m_viewSource, value); }
+
+        }
+        //public WebViewSource ViewSource { get; set; }
+    }
+}
