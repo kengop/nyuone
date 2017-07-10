11bcdb8282ec5404555fe788781d44e4b4d5c721
Kengo Inaba <kengop@gmail.com>
2017-07-11 00:41:08 +0900

    WebViewの追加

---
+++ nyu1/Views/WebView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using nyu1.ViewModels;
+using Xamarin.Forms;
+
+namespace nyu1.Views
+{
+    public partial class WebView : ContentPage
+    {
+        public WebView()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new WebViewModel();
+        }
+    }
+}
