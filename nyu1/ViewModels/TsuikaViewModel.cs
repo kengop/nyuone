351e7c0c0a6cbf2badf29fa0616152a6004e35cf
Kengo Inaba <kengop@gmail.com>
2017-08-13 23:39:55 +0900

    ローカルデータの読み書き動作確認用のコード追加

--- nyu1/ViewModels/TsuikaViewModel.cs
+++ nyu1/ViewModels/TsuikaViewModel.cs
@@ -1,30 +1,79 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Windows.Input;
 using nyu1.Helpers;
+
+using PCLStorage;
 
 namespace nyu1.ViewModels
 {
-	public class TsuikaViewModel : ObservableObject
-	{
+    public class TsuikaViewModel : ObservableObject
+    {
         #region Properties
 
         public string Hoge
-		{
-			get { return this.m_hoge; }
-			set { this.m_hoge = value; OnPropertyChanged(); }
-		}
+        {
+            get { return this.m_hoge; }
+            set { this.m_hoge = value; OnPropertyChanged(); }
+        }
 
-		public string m_hoge = "初期";
+        public string m_hoge = "初期";
+
+        private ICommand m_loadFileCommand;
+
+        public ICommand LoadFileCommand
+        {
+            get
+            {
+                return this.m_loadFileCommand;
+            }
+        }
+
+        private ICommand m_saveFileCommand;
+
+        public ICommand SaveFileCommand
+        {
+            get
+            {
+                return this.m_saveFileCommand;
+            }
+        }
+
+        private string m_localData;
+
+        public string LocalData
+        {
+            get
+            {
+                return this.m_localData;
+            }
+            set
+            {
+                this.SetProperty(ref this.m_localData, value);
+            }
+        }
 
         #endregion
 
         public TsuikaViewModel()
-		{
-		}
+        {
+            this.m_loadFileCommand = new Xamarin.Forms.Command(() => this.LoadFile());
+            this.m_saveFileCommand = new Xamarin.Forms.Command(() => this.SaveFile());
+            this.LocalData = "さしすせそ";
+        }
 
-		private void foo()
-		{
+        private async void SaveFile()
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFile file = await rootFolder.CreateFileAsync(
+                "data.txt",
+                CreationCollisionOption.ReplaceExisting);
+            await file.WriteAllTextAsync(this.LocalData);
+        }
 
-		}
-	}
+        private async void LoadFile()
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFile file = await rootFolder.GetFileAsync("data.txt");
+            this.LocalData = await file.ReadAllTextAsync();
+        }
+    }
 }
